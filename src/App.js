ab6004b530da96238ee16d60ebafe75d1d778daa
mohan062002 <2020bcs037@sggs.ac.in>
2023-04-15 23:03:18 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-
-
+import Main from './components/Main';
+import Nav from './components/Nav';
+import Login from './components/Login';
+import Signin from './components/Signin'
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        
-       <p className="text-xl font-mono text-red-600">Welcome to coding world</p>
+        <Nav/>
+        <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/Signin' element={<Signin/>}/>
+        </Routes>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div>welcome to main page</div>
+  )
+}
